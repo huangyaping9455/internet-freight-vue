868652df6f1805402510d4be99811439fec46cc9
tianjun <648711957@qq.com>
2021-01-22 15:39:05 +0800

    2021-1-22

---
+++ src/views/modules/capitalflow/capitalflow.vue
@@ -0,0 +1,250 @@
+<template>
+  <div class="mod-user">
+    <el-form :inline="true" :model="dataForm" @keyup.enter.native="getDataList()">
+      <el-form-item>
+        <el-input v-model="dataForm.driverName" placeholder="驾驶员姓名" clearable></el-input>
+      </el-form-item>
+<!--      <el-form-item>
+        <el-input v-model="dataForm.telephone" placeholder="电话号码" clearable></el-input>
+      </el-form-item>-->
+      <el-form-item>
+        <el-button @click="getDataList()">查询</el-button>
+        <el-button v-if="isAuth('post/admin/**')" type="primary" @click="addOrUpdateHandle()">新增</el-button>
+        <el-button v-if="isAuth('driver:internetdriver:delete')" type="danger" @click="deleteHandle()"
+                   :disabled="dataListSelections.length <= 0">批量删除
+        </el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="dataList"
+      :row-style="{height:'20px'}"
+      :cell-style="{padding:'0px'}"
+      border
+      v-loading="dataListLoading"
+      @selection-change="selectionChangeHandle"
+      style="width: 100%;">
+      <el-table-column
+        type="selection"
+        header-align="center"
+        align="center"
+        width="50">
+      </el-table-column>
+      <el-table-column
+        prop="driverName"
+        header-align="center"
+        align="center"
+        width="180"
+        label="姓名">
+      </el-table-column>
+      <el-table-column
+        prop="drivingLicense"
+        header-align="center"
+        align="center"
+        width="180"
+        label="驾驶证编号">
+      </el-table-column>
+      <el-table-column
+        prop="vehicleClass"
+        header-align="center"
+        align="center"
+        width="180"
+        label="准驾车型">
+      </el-table-column>
+      <el-table-column
+        prop="issuingOrganizations"
+        header-align="center"
+        align="center"
+        width="180"
+        label="驾驶证发证机关">
+      </el-table-column>
+      <el-table-column
+        prop="validPeriodFrom"
+        header-align="center"
+        align="center"
+        width="180"
+        label="驾驶证有效期自">
+      </el-table-column>
+      <el-table-column
+        prop="validPeriodTo"
+        header-align="center"
+        align="center"
+        width="180"
+        label="驾驶证有效期至">
+      </el-table-column>
+      <el-table-column
+        prop="qualificationCertificate"
+        header-align="center"
+        align="center"
+        width="180"
+        label="从业资格证号">
+      </el-table-column>
+      <el-table-column
+        prop="telephone"
+        header-align="center"
+        align="center"
+        width="180"
+        label="手机号码">
+      </el-table-column>
+      <el-table-column
+        prop="remark"
+        header-align="center"
+        align="center"
+        width="180"
+        label="备注">
+      </el-table-column>
+      <el-table-column
+        prop="driverLicense"
+        header-align="center"
+        align="center"
+        width="180"
+        label="驾驶证">
+      </el-table-column>
+      <el-table-column
+        prop="updateTime"
+        header-align="center"
+        align="center"
+        width="180"
+        label="更新时间">
+        <template slot-scope="scope">
+          {{ scope.row.updateTime | dateFormat }}
+        </template>
+      </el-table-column>
+      <el-table-column
+        fixed="right"
+        header-align="center"
+        align="center"
+        width="150"
+        label="操作">
+        <template slot-scope="scope">
+          <el-button v-if="isAuth('driver:internetdriver:update')" type="text" size="small"
+                     @click="addOrUpdateHandle(scope.row.id)">修改
+          </el-button>
+          <el-button v-if="isAuth('driver:internetdriver:delete')" type="text" size="small"
+                     @click="deleteHandle(scope.row.id)">
+            删除
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <el-pagination
+      @size-change="sizeChangeHandle"
+      @current-change="currentChangeHandle"
+      :current-page="pageIndex"
+      :page-sizes="[10, 20, 50, 100]"
+      :page-size="pageSize"
+      :total="totalPage"
+      :background="true"
+      layout="total, sizes, prev, pager, next, jumper">
+    </el-pagination>
+    <!-- 弹窗, 新增 / 修改 -->
+    <add-or-update v-if="addOrUpdateVisible" ref="addOrUpdate" @refreshDataList="getDataList"></add-or-update>
+  </div>
+</template>
+
+<script>
+import AddOrUpdate from './internetdriver-add-or-update'
+import {getDriverPage} from '@/api/api'
+
+export default {
+  data () {
+    return {
+      dataForm: {
+        driverName: ''//,
+        //telephone: ''
+      },
+      dataList: [],
+      pageIndex: 0,
+      pageSize: 20,
+      totalPage: 0,
+      dataListLoading: false,
+      dataListSelections: [],
+      addOrUpdateVisible: false
+    }
+  },
+  components: {
+    AddOrUpdate
+  },
+  activated () {
+    this.getDataList()
+  },
+  methods: {
+    // 获取数据列表
+    getDataList () {
+      this.dataListLoading = true
+      let params = {
+        'page': this.pageIndex,
+        'limit': this.pageSize,
+        'driverName': this.dataForm.driverName,
+        //'telephone': this.dataForm.telephone,
+        'organizationId': this.$store.state.user.organizationId
+      }
+      getDriverPage(params).then(({data}) => {
+        if (data && data.code === 0) {
+          let {data: {content, totalElements}} = data
+          this.totalPage = totalElements
+          this.dataList = content
+        } else {
+          this.dataList = []
+          this.totalPage = 0
+        }
+        this.dataListLoading = false
+      })
+    },
+    // 每页数
+    sizeChangeHandle (val) {
+      this.pageSize = val
+      this.pageIndex = 1
+      this.getDataList()
+    },
+    // 当前页
+    currentChangeHandle (val) {
+      this.pageIndex = val
+      this.getDataList()
+    },
+    // 多选
+    selectionChangeHandle (val) {
+      this.dataListSelections = val
+    },
+    // 新增 / 修改
+    addOrUpdateHandle (id) {
+      this.addOrUpdateVisible = true
+      this.$nextTick(() => {
+        this.$refs.addOrUpdate.init(id)
+      })
+    },
+    // 删除
+    deleteHandle (id) {
+      var ids = id ? [id] : this.dataListSelections.map(item => {
+       return item.id
+        alert(ids)
+      })
+      this.$confirm(`确定对[id=${ids.join(',')}]进行[${id ? '删除' : '批量删除'}]操作?`, '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        this.$http({
+          url: this.$http.addUrl(`/internetfreight/internetDrivers/${ids}`),
+          method: 'delete',
+        }).then(({data}) => {
+          if (data && data.code === 0) {
+            this.$message({
+              message: '操作成功',
+              type: 'success',
+              duration: 1500,
+              onClose: () => {
+                this.getDataList()
+              }
+            })
+            alert(message)
+            this.visible = true
+          } else {
+            this.$message.error(data.msg)
+          }
+        })
+      }).catch(() => {
+      })
+    }
+  }
+}
+</script>
---
+++ src/views/modules/capitalflow/internetdriver-add-or-update.vue
@@ -0,0 +1,190 @@
+<template>
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible">
+<!--   :rules="dataRule"-->
+    <el-form :model="dataForm"  ref="dataForm" @keyup.enter.native="dataFormSubmit()"
+             label-width="160px">
+      <el-form-item label="姓名" prop="driverName" :class="{ 'is-required': !dataForm.driverName }">
+        <el-input v-model="dataForm.driverName" placeholder="姓名"></el-input>
+      </el-form-item>
+      <el-form-item label="驾驶证编号" prop="drivingLicense" :class="{ 'is-required': !dataForm.drivingLicense }">
+        <el-input v-model="dataForm.drivingLicense" placeholder="驾驶证编号"></el-input>
+      </el-form-item>
+      <el-form-item label="准驾车型" prop="vehicleClass" :class="{ 'is-required': !dataForm.vehicleClass }">
+        <el-input v-model="dataForm.vehicleClass" placeholder="准驾车型"></el-input>
+      </el-form-item>
+      <el-form-item label="驾驶证发证机关" prop="issuingOrganizations"
+                    :class="{ 'is-required': !dataForm.issuingOrganizations }">
+        <el-input v-model="dataForm.issuingOrganizations" placeholder="驾驶证发证机关"></el-input>
+      </el-form-item>
+      <el-form-item label="驾驶证有效期自" prop="validPeriodFrom" :class="{ 'is-required': !dataForm.validPeriodFrom }">
+        <div class="block">
+          <el-date-picker
+            v-model="dataForm.validPeriodFrom"
+            type="date"
+            placeholder="选择日期">
+          </el-date-picker>
+        </div>
+      </el-form-item>
+      <el-form-item label="驾驶证有效期至" prop="validPeriodTo" :class="{ 'is-required': !dataForm.validPeriodTo }">
+        <div class="block">
+          <el-date-picker
+            v-model="dataForm.validPeriodTo"
+            type="date"
+            placeholder="选择日期">
+          </el-date-picker>
+        </div>
+      </el-form-item>
+      <el-form-item label="从业资格证号" prop="qualificationCertificate"
+                    :class="{ 'is-required': !dataForm.qualificationCertificate }">
+        <el-input v-model="dataForm.qualificationCertificate" placeholder="从业资格证号"></el-input>
+      </el-form-item>
+      <el-form-item label="手机号码" prop="telephone" :class="{ 'is-required': !dataForm.telephone }">
+        <el-input v-model="dataForm.telephone" placeholder="手机号码"></el-input>
+      </el-form-item>
+      <el-form-item label="备注" prop="remark">
+        <el-input v-model="dataForm.remark" placeholder="备注"></el-input>
+      </el-form-item>
+
+      <el-form-item label="驾驶证" prop="driverLicense">
+        <el-upload
+          class="upload-demo"
+          drag
+          action="filesystem/fileFastDFS/upload"
+          multiple>
+          <i class="el-icon-upload"></i>
+          <div class="el-upload__text" v-model="dataForm.driverLicense">将文件拖到此处，或<em>点击上传</em></div>
+          <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+        </el-upload>
+      </el-form-item>
+    </el-form>
+    <span slot="footer" class="dialog-footer">
+      <el-button @click="visible = false">取消</el-button>
+      <el-button type="primary" @click="dataFormSubmit()">确定</el-button>
+    </span>
+  </el-dialog>
+</template>
+
+<script>
+import {addDriver, updateDriverById} from '@/api/api'
+import {telephone} from '../../../utils/validate'
+
+export default {
+  data() {
+    return {
+      visible: false,
+      roleList: [],
+      dataForm: {
+        id: 0,
+        'driverName': '',
+        'drivingLicense': '',
+        'vehicleClass': '',
+        'issuingOrganizations': '',
+        'validPeriodFrom': '',
+        'validPeriodTo': '',
+        'qualificationCertificate': '',
+        'telephone': '',
+        'remark': '',
+        'driverLicense':'',
+        delete: 1
+      },
+      /*  dataRule: {
+    driverName: [
+         {required: true, message: '驾驶员姓名不能为空', trigger: 'blur'}
+       ],
+       drivingLicense: [
+         {required: true, message: '身份证号不能为空', trigger: 'blur'}
+       ],
+       vehicleClass: [
+         {required: true, message: '准驾车型不能为空', trigger: 'blur'}
+       ],
+       issuingOrganizations: [
+         {required: true, message: '驾驶证发证机关不能为空', trigger: 'blur'}
+       ],
+       validPeriodFrom: [
+         {required: true, message: '驾驶证有效期自不能为空', trigger: 'blur'}
+       ],
+       validPeriodTo: [
+         {required: true, message: '驾驶证有效期至不能为空', trigger: 'blur'}
+       ],
+       qualificationCertificate: [
+         {required: true, message: '从业资格证号不能为空', trigger: 'blur'}
+       ],
+      telephone: [
+        {required: true, message: '手机号不能为空', trigger: 'blur'},
+        {validator: telephone, trigger: 'blur'}
+      ]
+    }*/
+      /*日期*/
+      pickerOptions1: {
+        disabledDate(time) {
+          return time.getTime() > Date.now()
+        },
+      },
+      validPeriodFrom: '',
+      validPeriodTo: '',
+    }
+  },
+  methods: {
+    init(id) {
+      this.dataForm.id = id || 0
+      if (this.dataForm.id) {
+        this.$http({
+          url: this.$http.addUrl(`/internetfreight/financials/getOneById`),
+          method: 'get',
+          params: this.$http.addParams({id: this.dataForm.id})
+        }).then(({data}) => {
+          if (data && data.code === 0) {
+              this.dataForm.documentNumber = data.data.driverName,
+              this.dataForm.sendToProDateTime = data.data.drivingLicense,
+              this.dataForm.carrier = data.data.vehicleClass,
+              this.dataForm.actualCarrierId = data.data.issuingOrganizations,
+              this.dataForm.vehicleNumber = data.data.validPeriodFrom,
+              this.dataForm.vehiclePlateColorCode = data.data.validPeriodTo,
+              this.dataForm.remark = data.data.qualificationCertificate
+          }
+        })
+      }
+      this.visible = true
+    },
+    // 表单提交
+    dataFormSubmit() {
+      this.$refs['dataForm'].validate((valid) => {
+        let methods = `${!this.dataForm.id ? 'post' : 'put'}`;
+        if (valid) {
+          let dataForm = {
+            'id': this.dataForm.id || undefined,
+            'driverName': this.dataForm.driverName,
+            'drivingLicense': this.dataForm.drivingLicense,
+            'vehicleClass': this.dataForm.vehicleClass,
+            'issuingOrganizations': this.dataForm.issuingOrganizations,
+            'validPeriodFrom': this.dataForm.validPeriodFrom,
+            'validPeriodTo': this.dataForm.validPeriodTo,
+            'qualificationCertificate': this.dataForm.qualificationCertificate,
+            'telephone': this.dataForm.telephone,
+            'remark': this.dataForm.remark,
+            'driverLicense': this.dataForm.driverLicense
+          }
+          addDriver(dataForm, methods).then(({data}) => {
+            if (data && data.code === 0) {
+              this.$message({
+                message: '操作成功',
+                type: 'success',
+                duration: 1500,
+                onClose: () => {
+                  this.visible = false
+                  this.$emit('refreshDataList')
+                }
+              })
+            } else {
+              this.$message.error(data.msg)
+            }
+          })
+        }
+      })
+    }
+  }
+}
+</script>
