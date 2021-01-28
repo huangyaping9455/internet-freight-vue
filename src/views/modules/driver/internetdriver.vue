3c7d80c6eb0c72f371ff2395a40aa0ac19fa01d4
tianjun <648711957@qq.com>
2021-01-28 08:59:44 +0800

    2021-1-28

--- src/views/modules/driver/internetdriver.vue
+++ src/views/modules/driver/internetdriver.vue
@@ -153,7 +153,7 @@ export default {
         // telephone: ''
       },
       dataList: [],
-      pageIndex: 0,
+      pageIndex: 1,
       pageSize: 20,
       totalPage: 0,
       dataListLoading: false,
@@ -173,7 +173,7 @@ export default {
       this.dataListLoading = true
       const params = {
         page: this.pageIndex,
-        limit: this.pageSize,
+        limit: this. pageSize,
         driverName: this.dataForm.driverName,
         // 'telephone': this.dataForm.telephone,
         organizationId: this.$store.state.user.organizationId
